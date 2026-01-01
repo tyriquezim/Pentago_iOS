36d0d3d970b927997865ab99fde910e72e8db97b
tyriquezim <zimbizi.tyrique@gmail.com>
2026-01-01 22:41:02 +0800

    Added CustomTableView Cell types for the how to play screen. Successfully implement the PlayerProfile Select screen

---
+++ Pentago/Views/HowToPlayTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HowToPlayViewController.swift
+//  Pentago
+//
+//  Created by Tyrique Zimbizi on 1/1/2026.
+//
+import UIKit
+
+class HowToPlayViewController: UIViewController
+{
+    
+}
---
+++ Pentago/Views/ProfileSelectCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileSelectCollectionViewCell.swift
+//  Pentago
+//
+//  Created by Tyrique Zimbizi on 1/1/2026.
+//
+
+import UIKit
+
+class ProfileSelectCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet var profilePictureImageView: UIImageView!
+    @IBOutlet var usernameLabel: UILabel!
+}
